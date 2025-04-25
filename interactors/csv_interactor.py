986776b13df44c438ec9aea61a719a16ee341a7a
jfar0026 <farrandjonathan@gmail.com>
2025-04-25 22:08:14 +1000

    Added documentation

---
+++ interactors/csv_interactor.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from .request_interactor import RequestInteractor
+
+class CSVInteractor:
+    """
+    This class is responsible for interacting with csv file.\n
+    Author: Jonathan Farrand\n
+    Date: 2025-4-22
+    """
+    
+    @staticmethod
+    def get_csv(location: str) -> pd.DataFrame:
+        """
+        Reads a CSV file from the specified location and returns it as a pandas DataFrame.
+
+        :param location: The location of the CSV file (without extension).
+        
+        :return: A pandas DataFrame containing the data from the CSV file.
+        :raises ValueError: If the location is not a string or if the file is not a CSV file.
+        :raises FileNotFoundError: If the file is not found.
+        """
+        if type(location) != str:
+            raise ValueError("The location must be a string.")
+        try:
+            if location.endswith('.csv'):
+                with open(f'{location}', encoding='utf-8') as inputfile:
+                    return pd.read_csv(inputfile)
+            elif "." in location:
+                raise ValueError("The file is not a CSV file.")
+            else:
+                with open(f'{location}.csv', encoding='utf-8') as inputfile:
+                    return pd.read_csv(inputfile)
+        except FileNotFoundError:
+            raise FileNotFoundError
+    
+        
