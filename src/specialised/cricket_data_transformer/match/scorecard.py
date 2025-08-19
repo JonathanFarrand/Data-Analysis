a8437d2cb434da2a4cf6245a30183b00abf99b51
jfar0026 <farrandjonathan@gmail.com>
2025-08-19 18:57:27 +1000

    Created scorecard class

---
+++ src/specialised/cricket_data_transformer/match/scorecard.py
@@ -0,0 +1,17 @@
+
+
+
+class Scorecard:
+    """
+    A class to represent a cricket match scorecard.
+    It stores batting and bowling scores, and provides methods to add scores and retrieve them.
+    
+    Author: Jonathan Farrand
+    Date: 2025-08-19
+    """
+    def __init__(self):
+        self.bat_scores = dict()
+        self.bowl_scores = dict()
+        self.scores = []
+        
+        pass
